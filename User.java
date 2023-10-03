45a89113745d8a82d69dc8fff4d7d8475a15b72e
Om Patel <ompatel@Oms-MacBook-Pro-2.local>
2023-10-02 21:45:48 -0500

    User Proto

---
+++ User.java
@@ -0,0 +1,10 @@
+public class User {
+
+    private float capital;
+    private float suspicionOfSEC;                 
+    private double currentDebt;
+    private int stress;
+
+    private String[] stocks;
+    
+}

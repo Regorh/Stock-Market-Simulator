d32c42ae7d02d50078c3fa7c8a678ccb7e63ebd8
Ali Elnour <ali.elnour@slu.edu>
2023-10-05 18:51:31 -0500

    stock proto complete, but awaiting gradle for rand

--- model/Stock.java
+++ model/Stock.java
@@ -1,5 +1,21 @@
 package model;
 
 public class Stock {
-    
+    String name;
+    String type;
+    int id;
+    float price;
+    float stability;
+
+    public Stock(String name, String type, int id, float price) {
+        this.name = name;
+        this.type = type;
+        this.id = id;
+        this.price = price;
+        // TODO rand stability -- awaiting Gradle integration
+    }
+
+    float get_price() { return this.price; }
+
+    void set_price(float price) { this.set_price(price); }
 }

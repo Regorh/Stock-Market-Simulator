bda78bf7536d7bf5d43c10ca20d4471f9cf51219
nguzinski <nguzinski@slu.edu>
2023-10-10 17:54:43 -0500

    created market, stock list, randomization of stocks, random name generator

--- model/Market.java
+++ model/Market.java
@@ -1,5 +1,36 @@
 package model;
-
+import model.Stock;
+import java.util.Random;
 public class Market {
-    
+    int stability;
+    Random rand;
+    List<Stock> stocks;
+
+    public Market(){
+        Random rand = new Random()
+        stocks = new ArrayList<Stock>();
+        this.stability = rand.nextInt(4);
+        create_stocks(100);
+    }
+
+    public ArrayList get_stock() { return this.stocks; }
+
+    //creates stocks of an amount requested, with random values
+    private create_stocks(int num){
+        for( i = 0; i < num; i++){
+
+            Stock stock = new Stock(create_name(),rand.nextInt(4),(100*rand.nextDouble()));
+            this.stocks.add(stock);
+        }
+    }
+
+    //creates a random name when requested, 3 letters, for stock tickers
+    private String create_name(){
+        String name = "";
+        for(i = 0; i < 3; i++){
+            Character letter = Character.toString(rand.nextInt(24)+65);
+            name = name + letter;
+        }
+        return name;
+    }
 }

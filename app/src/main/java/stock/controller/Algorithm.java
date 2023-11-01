3ef081420aeb2101b8d23e9dd86d8e45c5477258
nguzinski <nguzinski@slu.edu>
2023-10-31 19:48:34 -0500

    fixed some minor mispellings and errors in the main

--- app/src/main/java/stock/controller/Algorithm.java
+++ app/src/main/java/stock/controller/Algorithm.java
@@ -6,10 +6,10 @@ import java.util.Random;
 public class Algorithm{
     Random rand = new Random();
 
-    public next_day(Market market){
+    public void next_day(Market market){
         List<Stock> list_of_stock = market.get_stock();
-        //need to call event roller here to decide what event soccur,
-        //this will help add weights to algortihm based on events
+        //need to call event roller here to decide what event occur,
+        //this will help add weights to algorithm based on events
         for( Stock stock : list_of_stock){
             stock_changer(stock);
             //BELOW HERE, we can check if the price has been falling a lot, then make stability lower
@@ -17,13 +17,13 @@ public class Algorithm{
 
     }
 
-    private stock_changer(Stock stock){
-        int rand_int = rand.nextDouble(1);
+    private void stock_changer(Stock stock){
+        double rand_int = rand.nextDouble(1);
         float current_price = stock.get_price();
         int current_stability = stock.get_stability();
 
         //will fill out with more weights as we go
-        float new_price(current_price+current_stability*(current_price(rand_int)))
+        float new_price = (current_price+current_stability*(current_price*(rand_int)))
 
         stock.set_price(new_price);
     }

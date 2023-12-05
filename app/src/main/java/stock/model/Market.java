23a661bc99c86e1c52d79b966843ea31a32f70b1
Ali Elnour <ali.elnour@slu.edu>
2023-12-04 21:35:02 -0600

    moved turn rollover stock modifications into market

--- app/src/main/java/stock/model/Market.java
+++ app/src/main/java/stock/model/Market.java
@@ -53,6 +53,23 @@ public class Market{
         return name.toString();
     }
 
+    public void change_stocks(float modify) {
+        for (Stock stock : this.stocks) {
+            stock_changer(modify, stock);
+        }
+    }
+
+    private void stock_changer(float modify, Stock stock){
+        double rand_double = rand.nextDouble(1);
+        float current_price = stock.get_price();
+        int current_stability = stock.get_stability();
+        int sign = (rand.nextFloat(0,1) <= .52)? 1 : -1;
+        //will fill out with more weights as we go
+        float new_price = modify + (float) ( current_price + (sign *current_stability * (current_price * (rand_double))));
+
+        stock.set_price(new_price);
+    }
+
     public void process_event(String event) {
         switch (event) {
             case "crash":
